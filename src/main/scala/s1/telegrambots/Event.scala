14f02f5db908a6a28c1a0cef724b7e9ae34b5741
Konsta <konsta.taipale@aalto.fi>
2021-10-10 21:30:49 +0300

    Uusia metodeja lisätty ja muokattu yhtä vanhaa Uusi muuttuja ja laitettu muutama vanha yksityisiksi

--- src/main/scala/s1/telegrambots/Event.scala
+++ src/main/scala/s1/telegrambots/Event.scala
@@ -2,19 +2,25 @@ package s1.telegrambots
 
 import scala.collection.mutable
 
-class Event(val tunnus: String, val nimi: String, val paikat: Int) {
+class Event(private val tapahtumatunnus: String, private val tapahtumanNimi: String, val paikat: Int) {
 
   private var ilmoittautuneideMaara = 0
   private var tapahtumaanIlmoittautuneet = mutable.Buffer[String]()
+  private var ilmoittautuneidenJono = mutable.Buffer[String]()
 
   // Palauttaa tosi jos henkilön nimi löytyy listalta
   def rekisteroitunut(henkilo: String) = this.tapahtumaanIlmoittautuneet.contains(henkilo)
 
   // Lisää henkilön tapahtumaan ilmoittautuneiden joukkoon
   def lisaaHenkilo(henkilo: String) = {
-    this.tapahtumaanIlmoittautuneet.append(henkilo)
-    this.ilmoittautuneideMaara += 1
+    if(tapahtumaanIlmoittautuneet.length < this.paikat) {
+      this.tapahtumaanIlmoittautuneet.append(henkilo)
+      this.ilmoittautuneideMaara += 1
+    } else {
+      this.ilmoittautuneidenJono.append(henkilo)
+    }
   }
+
 
   // Poistaa henkilön tapahtumaan ilmoittautuneiden joukosta
   def poistaHenkilo(henkilo: String) = {
@@ -24,5 +30,16 @@ class Event(val tunnus: String, val nimi: String, val paikat: Int) {
 
   // Palauttaa ilmoittautuneiden määrän
   def ilmoittautuneet = this.ilmoittautuneideMaara
+
+  // Palauttaa vapaina olevien paikkojen määrän
+  def paikkojaVapaana = {
+    this.paikat - this.ilmoittautuneideMaara
+  }
+
+  // Palauttaa tapahtuman tunnuksen
+  def tunnus = this.tapahtumatunnus
+
+  // Palauttaa tapahtuman nimen
+  def nimi = this.tapahtumanNimi
 
 }
