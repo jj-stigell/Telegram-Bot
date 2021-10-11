6f1422be9bdfd334ce67ab02dfc00cb53c722fdc
Satama Pyry <pyry.satama@aalto.fi>
2021-10-11 15:00:20 +0300

    poistaHenkilo:n lisätty viesti

--- src/main/scala/s1/telegrambots/Event.scala
+++ src/main/scala/s1/telegrambots/Event.scala
@@ -28,6 +28,7 @@ class Event(private val tapahtumatunnus: String, private val tapahtumanNimi: Str
     this.ilmoittautuneideMaara -= 1
     if(this.ilmoittautuneidenJono.nonEmpty) {
       this.tapahtumaanIlmoittautuneet.append(ilmoittautuneidenJono.head)
+      s"${ilmoittautuneidenJono.head} on lisätty tapahtumaan."
       this.ilmoittautuneideMaara += 1
       this.ilmoittautuneidenJono.remove(0)
     }
